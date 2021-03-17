6dcb4c391aff01cd2f25ead1fa0848784a4d3540
aosman <pianogreen0@gmail.com>
2021-03-17 11:46:22 +0000

    Ready for deployment

--- server/config/environment.py
+++ server/config/environment.py
@@ -1,2 +1,3 @@
-db_URI = 'postgres://localhost:5432/barter_db'
-secret = 'vjkh45kjhekghknhe5knvknkl5thvzhahgqn'
+import os
+db_URI = os.getenv('DATABASE_URL', 'postgres://localhost:5432/barter_db')
+secret = os.getenv('SECRET', 'vjkh45kjhekghknhe5knvknkl5thvzhahgqn')
